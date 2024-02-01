36e168750574be5d598022aea8db2d2b4f1ba1ba
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2024-02-01 11:24:23 -0600

    fixed axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:'https://e75f-68-235-144-167.ngrok-free.app',
-    headers:{ "ngrok-skip-browser-warning" : "true", "Access-Control-Allow-Origin":"*"}
+    headers:{ "ngrok-skip-browser-warning" : "true"}
 });
 
 export default api;
